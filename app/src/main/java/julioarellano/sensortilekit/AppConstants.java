1947110736347bd14030b013e7ed311a66bbfb54
jcarellanov <jcarellanov@gmail.com>
2017-08-26 14:24:43 +0200

    +Mod: adding new layouts and login activity

---
+++ app/src/main/java/julioarellano/sensortilekit/AppConstants.java
@@ -0,0 +1,8 @@
+package julioarellano.sensortilekit;
+
+
+public class AppConstants {
+
+    public static final String nodeTag = "node_tag";
+
+}
---
+++ app/src/main/java/julioarellano/sensortilekit/LoginActivity.java
@@ -0,0 +1,15 @@
+package julioarellano.sensortilekit;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
